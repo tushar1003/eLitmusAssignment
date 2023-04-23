9ad19804f3d67194a6eb091dae50ad566353cce3
tushar1003 <91830386+tushar1003@users.noreply.github.com>
2023-04-23 18:06:05 +0530

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+# Elitmus.github.io
---
+++ logscript.js
@@ -0,0 +1,55 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAUc8aTYhkk_Vl-3j9p37IJmgxxLoa7fmM",
+    authDomain: "elitmus-game.firebaseapp.com",
+    projectId: "elitmus-game",
+    storageBucket: "elitmus-game.appspot.com",
+    messagingSenderId: "49199503587",
+    appId: "1:49199503587:web:9e6026a13c69edd2364dd6",
+    measurementId: "G-463MLWLH2W"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  const auth= getAuth()
+
+  //console.log(app);
+
+  document.getElementById("login").addEventListener("click", function() {
+    var email =  document.getElementById("email").value;
+    var password = document.getElementById("passwords").value;
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+      alert(user.email+" Login successfully!!!");
+      window.location.href = "index.html";
+      
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      alert(errorMessage);
+    });		  		  
+});
+
+
+
+
+
+  
